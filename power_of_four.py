169359bc5fff45f7131bc4575c0a77eddabdf2c9
Priyanjani Chandra <47119235+PriyanjaniCh@users.noreply.github.com>
2020-08-04 16:39:33 -0500

    Update power_of_four.py

--- power_of_four.py
+++ power_of_four.py
@@ -4,6 +4,9 @@
 # Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
 # Follow up: Could you solve it without loops/recursion?
 
+# log num to base 4 - if integer (2.0) true else (2.4) false
+# num should be equal to 4 to the power of log value - then true
+# Can be done using recursion too 
 
 class Solution:
     def isPowerOfFour(self, num: int) -> bool:
